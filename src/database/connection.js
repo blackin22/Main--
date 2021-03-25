f17339beb1f43c13914a864dd105424e824ef8c7
Blackin22 <77985817+blackin22@users.noreply.github.com>
2021-03-25 13:47:18 -0300

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,14 +4,14 @@
   	start(){
   		try {
         var firebaseConfig = {
-         apiKey: "AIzaSyCBOcwZpk7AK_SKeYZA24yILEohpdW9RNg",
-         authDomain: "motoko-add44.firebaseapp.com",
-         databaseURL: "https://motoko-add44-default-rtdb.firebaseio.com",
-         projectId: "motoko-add44",
-         storageBucket: "motoko-add44.appspot.com",
-         messagingSenderId: "282958549269",
-         appId:"1:282958549269:web:669bba8ef746c6dd76191c",
-         measurementId: "G-SCY89B43T0"
+         apiKey: "",
+         authDomain: "",
+         databaseURL: "",
+         projectId: "",
+         storageBucket: "",
+         messagingSenderId: "",
+         appId:"",
+         measurementId: ""
                 };
               firebase.initializeApp(firebaseConfig);
 
